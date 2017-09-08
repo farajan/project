40705c260b55ad175f97267614694b07f0af2434
farajan <jan.fara10@gmail.com>
2017-09-08 12:28:23 +0200

    add new list

--- src/app/component/addlist/addlist.component.ts
+++ src/app/component/addlist/addlist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef, NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-addlist',
@@ -8,14 +9,23 @@ import { NgbModalRef, NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-boo
 })
 export class AddlistComponent implements OnInit {
 
+  items: FirebaseListObservable<any[]>;
   private modalWindow: NgbModalRef;
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal,
+    public db: AngularFireDatabase) {
+    this.items = db.list('/items');
+  }
 
   ngOnInit() {
   }
-  
+
   open(content) {
     this.modalWindow = this.modalService.open(content);
+  }
+
+  private addList(name: string) {
+    this.items.push({ value: name });
+    this.modalWindow.close();
   }
 }
